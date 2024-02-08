0e2039ec541de8233f5eec79f03b46955feeb94e
zingo10 <151099896+zingo10@users.noreply.github.com>
2024-02-08 13:51:29 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13960 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,9 +1,8 @@
-     
 public class Main{
     public static void main(String[] args){
-        System.out.println("\\    /\\\n"+
-                         " )  ( ')\n"+
-                         "(  /  )\n"+
-                         " \\(__)|");
+        System.out.print("\\    /\\\n"+
+                          " )  ( ')\n"+
+                          "(  /  )\n"+          
+                          " \\(__)|");
     }
 }
